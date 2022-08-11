9d2fa4b3599c4a8b21d26e8f14dfa8c02804a9b1
jeremy <poohc300@gmail.com>
2022-08-11 17:02:07 +0900

    JM [Feat] 환자 검색, 삽입, 수정 기능 구현

--- src/main/java/com/example/demo/nurse/repository/NurseRepository.java
+++ src/main/java/com/example/demo/nurse/repository/NurseRepository.java
@@ -1,8 +1,35 @@
 package com.example.demo.nurse.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.vo.PatientVo;
+
 
 @Repository
 public class NurseRepository {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	public List<PatientVo> findAllPatients() {
+		return sqlSession.selectList("nurse.findAllPatients");
+	}
+
+	public PatientVo findPatientByNo(Long no) {
+		return sqlSession.selectOne("nurse.findPatientByNo", no);
+	}
+
+	public Boolean insertPatient(PatientVo patientVo) {
+		return sqlSession.insert("nurse.insertPatient", patientVo) == 1;
+	}
+
+
+	public Boolean updatePatient(PatientVo patientVo) {
+		return sqlSession.update("nurse", patientVo) == 1;		
+	}
 
 }
--- src/main/java/com/example/demo/nurse/service/NurseService.java
+++ src/main/java/com/example/demo/nurse/service/NurseService.java
@@ -1,8 +1,42 @@
 package com.example.demo.nurse.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.nurse.repository.NurseRepository;
+import com.example.demo.vo.PatientVo;
+
 
 @Service
 public class NurseService {
 
+	@Autowired
+	private NurseRepository nurseRepository;
+
+	public List<PatientVo> getHospitalList() {
+		return nurseRepository.findAllPatients();
+	}
+
+	public PatientVo getHospitalOne(Long no) {
+		return nurseRepository.findPatientByNo(no);
+	}
+
+	@Transactional
+	public Boolean patientJoin(PatientVo patientVo) {
+		nurseRepository.insertPatient(patientVo);
+
+		
+		return nurseRepository.insertPatient(patientVo);
+	}
+
+
+	@Transactional
+	public Boolean patientUpdate(PatientVo patientVo) {
+		nurseRepository.updatePatient(patientVo);
+
+		return nurseRepository.updatePatient(patientVo);
+	}
 }
